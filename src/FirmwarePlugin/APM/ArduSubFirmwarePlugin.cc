56adbb2fbdf4412f810b920f3de5cadb3cdb03d0
Eliot-Insight <eliot@insightinfra.com.au>
2021-03-05 14:17:44 +1100

    Working top computer code

--- src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.cc
+++ src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.cc
@@ -1,17 +1,24 @@
 /*=====================================================================
+
  QGroundControl Open Source Ground Control Station
+
  (c) 2009 - 2015 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+
  This file is part of the QGROUNDCONTROL project
+
  QGROUNDCONTROL is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
+
  QGROUNDCONTROL is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
+
  You should have received a copy of the GNU General Public License
  along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+
  ======================================================================*/
 
 /// @file
@@ -199,7 +206,7 @@ const QVariantList& ArduSubFirmwarePlugin::modeIndicators(const Vehicle* vehicle
     Q_UNUSED(vehicle);
     //-- Sub specific list of indicators (Enter your modified list here)
     if(_modeIndicators.size() == 0) {
-      _modeIndicators = QVariantList({
+        _modeIndicators = QVariantList({
             QVariant::fromValue(QUrl::fromUserInput("qrc:/toolbar/ModeIndicator.qml")),
             QVariant::fromValue(QUrl::fromUserInput("qrc:/toolbar/ArmedIndicator.qml")),
         });
--- src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/ArduSubFirmwarePlugin.h
@@ -1,17 +1,24 @@
 /*=====================================================================
+
  QGroundControl Open Source Ground Control Station
+
  (c) 2009 - 2014 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+
  This file is part of the QGROUNDCONTROL project
+
  QGROUNDCONTROL is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
+
  QGROUNDCONTROL is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
+
  You should have received a copy of the GNU General Public License
  along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+
  ======================================================================*/
 
 /// @file
@@ -33,7 +40,7 @@ public:
     Q_PROPERTY(Fact* lightsLevel1        READ lightsLevel1        CONSTANT)
     Q_PROPERTY(Fact* lightsLevel2        READ lightsLevel2        CONSTANT)
     Q_PROPERTY(Fact* pilotGain           READ pilotGain           CONSTANT)
-    Q_PROPERTY(Fact* inputHold           READ inputHold           CONSTANT)
+    Q_PROPERTY(Fact* inputHold           READ inputHold     CONSTANT)
     Q_PROPERTY(Fact* utGauge             READ utGauge             CONSTANT) // Added UT Gauge
     Q_PROPERTY(Fact* rangefinderDistance READ rangefinderDistance CONSTANT)
 
